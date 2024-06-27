ce79e84ed95fbaa452e4dc69f5a0da6c821657b0
Siddharth <siddharthsakre@gmail.com>
2024-06-27 09:03:56 +0100

    Add websockets listeners to listen for IOT trigger events

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import MQTTServer from './mqtt-server.js';
-const PORT = 1883;
-
-const mqttServerInstance = new MQTTServer(PORT);
+const MQTT_PORT = 1883;
+const WS_PORT = 9999;
+const mqttServerInstance = new MQTTServer(MQTT_PORT, WS_PORT);
 
 process.on('SIGINT', () => {
   console.log('Shutting down server...');
--- mqtt-server.js
+++ mqtt-server.js
@@ -1,105 +1,134 @@
-// MQTTServer.js
-
 import aedes from 'aedes';
 import { createServer } from 'net';
 import mqtt from 'mqtt';
 import { Subject } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
+import { Server as SocketIOServer } from 'socket.io';
+import http from 'http';
 
 class MQTTServer {
-  constructor(port) {
+  constructor(port, wsPort) {
     this.port = port;
+    this.wsPort = wsPort;
     this.mqttServer = aedes();
-    this.serverInstance = createServer(this.mqttServer.handle);
+    this.tcpServer = createServer(this.mqttServer.handle);
+    this.httpServer = http.createServer();
+    this.io = new SocketIOServer(this.httpServer, { cors: { origin: '*' } });
     this.mqttClient = null;
     this.subscribers = {};
+    this.wsClients = new Set();
 
     this.clientConnected$ = new Subject();
     this.clientDisconnected$ = new Subject();
     this.messagePublished$ = new Subject();
     this.clientSubscribed$ = new Subject();
     this.clientUnsubscribed$ = new Subject();
+    this.webSocketClients = {};
 
     this.init();
   }
 
   init() {
-    this.serverInstance.listen(this.port, () => {
-      this.mqttClient = mqtt.connect(`mqtt://localhost:${this.port}`);
-      console.log(`Server started and listening on port ${this.port}`);
+    this.tcpServer.listen(this.port, () => {
+      this.mqttClient = mqtt.connect(`mqtt://localhost:${this.port}`, {
+        clientId: 'MQTT_SERVER_CLIENT',
+      });
+      console.log(`MQTT server started and listening on TCP port ${this.port}`);
     });
 
+    this.httpServer.listen(this.wsPort, () => {
+      console.log(`WebSocket (socket.io) server started and listening on port ${this.wsPort}`);
+    });
+
+    this.setupSocketIO();
     this.setupSubscriptions();
     this.setupEventHandlers();
+  }
+
+  setupSocketIO() {
+    this.io.on('connection', (socket) => {
+      console.log('New client connected via WebSocket', socket.id);
+      this.webSocketClients[socket.id] = socket;
+
+      socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        delete this.webSocketClients[socket.id];
+      });
+
+      socket.on('IOT_DEVICES_REQUEST', (data) => {
+        this.webSocketClients[data.id]?.emit('IOT_DEVICES', this.subscribers);
+      });
+
+      socket.on('IOT_TRIGGER', (data) => {
+        const { topic, payload } = data;
+        this.publishMessage(topic, JSON.stringify(payload));
+      });
+    });
+  }
+
+  broadcastIotSubscribers() {
+    console.log('Current Subscribers:', JSON.stringify(this.subscribers, null, 2));
   }
 
   publishMessage(topic, message) {
     this.mqttClient.publish(topic, message);
   }
 
+  handleClientConnected(client) {
+    console.log(`New client connected: ${client.id}`);
+  }
+
+  handleClientDisconnected(client) {
+    console.log(`Client disconnected: ${client.id}`);
+
+    Object.keys(this.subscribers).forEach((topic) => {
+      if (this.subscribers[client.id]) {
+        delete this.subscribers[topic];
+      }
+    });
+
+    this.broadcastIotSubscribers();
+  }
+
+  handleMessagePublished({ packet, client }) {
+    if (client && client.id !== 'MQTT_SERVER_CLIENT') {
+      console.log(`Message published by ${client.id}:`);
+      const payload = packet.payload.toString() ? JSON.parse(packet.payload.toString()) : {};
+      this.subscribers[client.id] = { ...this.subscribers[client.id], ...payload };
+    }
+  }
+
+  handleClientSubscribed({ subscriptions, client }) {
+    subscriptions.forEach((sub) => {
+      console.log(`Client ${client.id} subscribed to ${sub.topic}`);
+      this.subscribers[client.id] = { ...(this.subscribers[client.id] || {}), topic: sub.topic };
+    });
+    this.broadcastIotSubscribers();
+  }
+
+  handleClientUnsubscribed({ subscriptions, client }) {
+    subscriptions.forEach((sub) => {
+      console.log(`Client ${client.id} unsubscribed from ${sub}`);
+    });
+    this.broadcastIotSubscribers();
+  }
+
   setupSubscriptions() {
-    this.clientConnected$
-      .pipe(tap((client) => console.log(`New client connected: ${client.id}`)))
-      .subscribe();
+    this.clientConnected$.pipe(tap((client) => this.handleClientConnected(client))).subscribe();
 
     this.clientDisconnected$
-      .pipe(
-        tap((client) => {
-          console.log(`Client disconnected: ${client.id}`);
-          Object.keys(this.subscribers).forEach((topic) => {
-            this.subscribers[topic] = this.subscribers[topic].filter((id) => id !== client.id);
-            if (this.subscribers[topic].length === 0) {
-              delete this.subscribers[topic];
-            }
-          });
-        }),
-        tap(() => this.logSubscribers())
-      )
+      .pipe(tap((client) => this.handleClientDisconnected(client)))
       .subscribe();
 
     this.messagePublished$
       .pipe(
         filter(({ client }) => !!client),
-        tap(({ packet, client }) => {
-          console.log(`Message published by ${client.id}:`, packet.payload.toString());
-        })
+        tap((event) => this.handleMessagePublished(event))
       )
       .subscribe();
 
-    this.clientSubscribed$
-      .pipe(
-        tap(({ subscriptions, client }) => {
-          subscriptions.forEach((sub) => {
-            console.log(`Client ${client.id} subscribed to ${sub.topic}`);
-            this.publishMessage(sub.topic, JSON.stringify({ type: 'LED_EFFECT', EFFECT_NO: 71 }));
-            if (!this.subscribers[sub.topic]) {
-              this.subscribers[sub.topic] = [];
-            }
-            if (!this.subscribers[sub.topic].includes(client.id)) {
-              this.subscribers[sub.topic].push(client.id);
-            }
-          });
-        }),
-        tap(() => this.logSubscribers())
-      )
-      .subscribe();
-
-    this.clientUnsubscribed$
-      .pipe(
-        tap(({ subscriptions, client }) => {
-          subscriptions.forEach((sub) => {
-            console.log(`Client ${client.id} unsubscribed from ${sub}`);
-            if (this.subscribers[sub]) {
-              this.subscribers[sub] = this.subscribers[sub].filter((id) => id !== client.id);
-              if (this.subscribers[sub].length === 0) {
-                delete this.subscribers[sub];
-              }
-            }
-          });
-        }),
-        tap(() => this.logSubscribers())
-      )
-      .subscribe();
+    this.clientSubscribed$.pipe(tap((event) => this.handleClientSubscribed(event))).subscribe();
+    this.clientUnsubscribed$.pipe(tap((event) => this.handleClientUnsubscribed(event))).subscribe();
   }
 
   setupEventHandlers() {
@@ -114,10 +143,6 @@ class MQTTServer {
     this.mqttServer.on('unsubscribe', (subscriptions, client) =>
       this.clientUnsubscribed$.next({ subscriptions, client })
     );
-  }
-
-  logSubscribers() {
-    console.log('Current Subscribers:', JSON.stringify(this.subscribers, null, 2));
   }
 
   getSubscribers() {
